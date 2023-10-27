c949b315c7ffef8e60bbef056744334d9f8174de
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2023-10-27 21:54:42 +0100

    Mover and unit spec

---
+++ robot_mover.rb
@@ -0,0 +1,98 @@
+
+class RobotMover
+  DIRECTIONS = %w[NORTH EAST SOUTH WEST].freeze
+  TABLE_MAX = 4
+  F_MAX = DIRECTIONS.length - 1
+  CLOCKWISE = FORWARD = 1
+  COUNTER_CLOCKWISE = BACK = -1
+
+  RobotPosition = Struct.new(:x, :y, :f) do
+    def valid?
+      values.all? { |value| value.is_a? Integer } &&
+        x.between?(0, TABLE_MAX) &&
+        y.between?(0, TABLE_MAX) &&
+        f.between?(0, F_MAX)
+    end
+
+    def horizontal_facing?
+      %w[NORTH SOUTH].include? DIRECTIONS[f]
+    end
+
+    def positive_facing?
+      %w[NORTH EAST].include? DIRECTIONS[f]
+    end
+
+    def facing
+      DIRECTIONS[f].capitalize
+    end
+  end
+
+  def unplaced_new_robot
+    RobotPosition.new
+  end
+
+  def report(position)
+    return position unless position.valid?
+
+    printf <<~HEREDOC
+      The robot is facing #{position.facing} at #{position.x}, #{position.y}.
+      #{position_map(position)}
+    HEREDOC
+
+    position
+  end
+
+  def place(initial_position = unplaced_new_robot, x:, y:, facing:)
+    new_position = RobotPosition.new(x: x, y: y, f: DIRECTIONS.index(facing))
+    new_position.valid? ? new_position : initial_position
+  end
+
+  def move(initial_position)
+    return initial_position unless initial_position.valid?
+
+    new_position = initial_position.dup
+    axis = initial_position.horizontal_facing? ? :y : :x
+    direction = initial_position.positive_facing? ? FORWARD : BACK
+
+    new_position[axis] = initial_position[axis] + direction
+    new_position.valid? ? new_position : initial_position
+  end
+
+  def left(initial_position)
+    return initial_position unless initial_position.valid?
+
+    turn(initial_position, COUNTER_CLOCKWISE)
+  end
+
+  def right(initial_position)
+    return initial_position unless initial_position.valid?
+
+    turn(initial_position, CLOCKWISE)
+  end
+
+  private
+
+  def position_map(position)
+    table = ''
+    [*0..TABLE_MAX].reverse.each do |row|
+      if position.y == row
+        [*0..TABLE_MAX].each do |col|
+          table << if col == position.x
+                     'R '
+                   else
+                     '■ '
+                   end
+        end
+      else
+        table << '■ ' * 5 unless position.y == row
+      end
+      table << "\n"
+    end
+    table
+  end
+
+  def turn(position, direction)
+    position.f = (position.f + direction) % DIRECTIONS.length
+    position
+  end
+end
---
+++ robot_mover_spec.rb
@@ -0,0 +1,84 @@
+require 'minitest/autorun'
+require './robot_mover'
+
+describe RobotMover do
+  before do
+    @robot_mover = RobotMover.new
+    @unplaced_robot = @robot_mover.unplaced_new_robot
+  end
+
+  describe "#place" do
+    it "does not put the robot off the table" do
+      @robot_mover.place(x: RobotMover::TABLE_MAX + 2, y: 0, facing: "NORTH").values.must_equal [nil, nil, nil]
+    end
+
+    it "places the robot on the table" do
+      @robot_mover.place(x: 0, y: 0, facing: "NORTH").values.must_equal [0, 0, 0]
+    end
+  end
+
+  describe "left" do
+    it "does not turn if not placed" do
+      @robot_mover.left(@unplaced_robot).must_equal @unplaced_robot
+    end
+
+    it "turns counter-clockwise" do
+      north_facing_robot = @robot_mover.place(x: 0, y: 0, facing: "NORTH")
+      west_facing_robot = @robot_mover.left(north_facing_robot)
+      west_facing_robot.facing.must_equal "West"
+
+      south_facing_robot = @robot_mover.left(west_facing_robot)
+      south_facing_robot.facing.must_equal "South"
+
+      east_facing_robot = @robot_mover.left(south_facing_robot)
+      east_facing_robot.facing.must_equal "East"
+
+      @robot_mover.left(east_facing_robot).facing.must_equal "North"
+    end
+  end
+
+  describe "#right" do
+    it "does not turn if not placed" do
+      @robot_mover.right(@unplaced_robot).must_equal @unplaced_robot
+    end
+
+    it "turns the robot clockwise" do
+      north_facing_robot = @robot_mover.place(x: 0, y: 0, facing: "NORTH")
+      east_facing_robot = @robot_mover.right(north_facing_robot)
+      east_facing_robot.facing.must_equal "East"
+
+      south_facing_robot = @robot_mover.right(east_facing_robot)
+      south_facing_robot.facing.must_equal "South"
+
+      west_facing_robot = @robot_mover.right(south_facing_robot)
+      west_facing_robot.facing.must_equal "West"
+
+      @robot_mover.right(west_facing_robot).facing.must_equal "North"
+    end
+  end
+
+  describe "#move" do
+    it "does not move anywhere if not placed" do
+      @robot_mover.move(@unplaced_robot).must_equal @unplaced_robot
+    end
+
+    it "does not fall off the table" do
+      placed_robot = @robot_mover.place(x: 0, y: 0, facing: "SOUTH")
+      @robot_mover.move(placed_robot).must_equal placed_robot
+    end
+
+    it "moves in the direction the robot is facing" do
+      north_facing_robot = @robot_mover.place(x: 1, y: 1, facing: "NORTH")
+      @robot_mover.move(north_facing_robot).values.must_equal [1, 2, 0]
+
+      east_facing_robot = @robot_mover.place(x: 1, y: 1, facing: "EAST")
+      @robot_mover.move(east_facing_robot).values.must_equal [2, 1, 1]
+
+      south_facing_robot = @robot_mover.place(x: 1, y: 1, facing: "SOUTH")
+      @robot_mover.move(south_facing_robot).values.must_equal [1, 0, 2]
+
+      west_facing_robot = @robot_mover.place(x: 1, y: 1, facing: "WEST")
+      @robot_mover.move(west_facing_robot).values.must_equal [0, 1, 3]
+    end
+  end
+end
